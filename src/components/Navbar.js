38de6b4643da482c62b20dc4060bce59eb56f404
Tanay Van <tanayvan258@gmail.com>
2020-06-28 09:10:56 +0530

    Navbar Link Working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./footer.css";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
 import logo from "../assets/logo.png";
 import { useTranslation } from "react-i18next";
 import LaunguageSelector from "./LaunguageSelector";
-export default function Navbar() {
+
+const currentTab = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#ed1c24" };
+  } else {
+    return { color: "black" };
+  }
+};
+
+const Navbar = ({ history }) => {
   const { t, i18n } = useTranslation();
   return (
     <div>
@@ -32,7 +41,11 @@ export default function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav ml-auto">
-            <Link className="nav-item nav-link active" to="/">
+            <Link
+              className="nav-item nav-link active"
+              to="/"
+              style={currentTab(history, "/")}
+            >
               {t("home.label")} <span className="sr-only">(current)</span>
             </Link>
             <div class="dropdown show">
@@ -58,10 +71,18 @@ export default function Navbar() {
             <a className="nav-item nav-link" href="#">
               {t("weare.label")}
             </a>
-            <Link className="nav-item nav-link" to="/about-us">
+            <Link
+              className="nav-item nav-link"
+              to="/about-us"
+              style={currentTab(history, "/about-us")}
+            >
               {t("about.label")}
             </Link>
-            <Link className="nav-item nav-link" to="/contact-us">
+            <Link
+              className="nav-item nav-link"
+              to="/contact-us"
+              style={currentTab(history, "/contact-us")}
+            >
               {t("contactus.label")}
             </Link>
           </div>
@@ -69,4 +90,6 @@ export default function Navbar() {
       </nav>
     </div>
   );
-}
+};
+
+export default withRouter(Navbar);

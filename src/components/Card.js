2f2e4bd6a5a69af8718f40a9ba922ba168f7635e
Rohit <varsadarohit@gmail.com>
2020-06-29 13:22:52 +0530

    home page edited

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,11 @@
 import React from "react";
 import "./card.css";
 
-function Tiles({ className, title, subtitle }) {
+function Tiles({ className, title, subtitle, image }) {
   return (
     <div className={className}>
       <div className="tiles">
-        <img
-          className="image"
-          width={"100%"}
-          src="https://images.unsplash.com/photo-1511467687858-23d96c32e4ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-          style={{}}
-        />
+        <img className="image" width={"100%"} src={image} />
         <div className="text-container ml-4 mt-3">
           <h4 className="title">{title}</h4>
           <p className="description text-muted">{subtitle}</p>
